78df134ef525cd1f66316db23840c3869447a3f2
Anson Wang <tw2730@columbia.edu>
2019-12-09 17:45:18 +0000

    Edit the urls sequence and add botton to stats page

--- sightings/urls.py
+++ sightings/urls.py
@@ -6,8 +6,8 @@ from . import views
 app_name = 'sightings'
 urlpatterns = [
         path('',views.sightings_view,name='sightings'),
-        path('<str:sq_id>',views.edit_view,name='edit'),
+        path('stats/',views.stats,name='stats'),
         path('add/', views.add_view.as_view(), name='add'),
         path('<str:pk>/delete/', views.delete_view.as_view(),name='delete'),
-        path('stats/',views.stats,name='stats'),
+        path('<str:sq_id>/',views.edit_view,name='edit'),
         ]
